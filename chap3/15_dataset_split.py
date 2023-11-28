a2352d8a9478093000cc1d4c873b69b603186dcc
Calli-T <Calli-T@naver.com>
2023-11-28 21:46:23 +0900

    data split

--- chap3/15_dataset_split.py
+++ chap3/15_dataset_split.py
@@ -1,8 +1,12 @@
+# 미완성
+
 import torch
 import pandas as pd
 from torch import nn
 from torch import optim
 from torch.utils.data import Dataset, DataLoader
+
+from torch.utils.data import random_split
 
 import torch_directml
 
@@ -10,7 +14,6 @@ device = torch_directml.device() if torch_directml.is_available() else "cpu"
 print(device)
 
 
-# torch dataset을 상속한 사용자 정의 데이터세트
 class CustomDataset(Dataset):
     def __init__(self, file_path):
         df = pd.read_csv(file_path)
@@ -18,7 +21,6 @@ class CustomDataset(Dataset):
         self.y = df.iloc[:, 1].values
         self.length = len(df)
 
-    # 이차방정식이므로....
     def __getitem__(self, index):
         x = torch.FloatTensor([self.x[index] ** 2, self.x[index]])
         y = torch.FloatTensor([self.y[index]])
@@ -28,7 +30,6 @@ class CustomDataset(Dataset):
         return self.length
 
 
-# 모델 초기화와 스탭당 행동정의
 class CustomModel(nn.Module):
     def __init__(self):
         super().__init__()
@@ -39,11 +40,25 @@ class CustomModel(nn.Module):
         return x
 
 
-# 데이터 셋과 데이터 로더
+'''
 train_dataset = CustomDataset("nonlinear.csv")
 train_dataloader = DataLoader(train_dataset, batch_size=2, shuffle=True, drop_last=True)
+'''
+dataset = CustomDataset('nonlinear.csv')
+dataset_size = len(dataset)
+train_size = int(dataset_size * 0.8)
+validation_size = int(dataset_size * 0.1)
+test_size = dataset_size - train_size - validation_size
 
-# 모델, 손실함수, 최적화
+# ---------------------------------------------------------
+train_dataset, validation_dataset, test_dataset = random_split(dataset, [train_size, validation_size, test_size])
+# random_split(데이터셋, [길이1, 길이2...])
+# 길이 배열 데이터 총합과 같아야함
+train_dataloader = DataLoader(train_dataset, batch_size=2, shuffle=True, drop_last=True)
+validation_dataloader = DataLoader(validation_dataset, batch_size=2, shuffle=True, drop_last=True)
+test_dataloader = DataLoader(test_dataset, batch_size=2, shuffle=True, drop_last=True)
+# ---------------------------------------------------------
+
 model = CustomModel()
 criterion = nn.MSELoss().to(device)
 optimizer = optim.SGD(model.parameters(), lr=0.0001)
@@ -53,13 +68,10 @@ for epoch in range(10000):
     cost = 0.0
 
     for x, y in train_dataloader:
-        # device에 맞는 벡터로
         x = x.to(device)
         y = y.to(device)
 
-        # 값에 넣고
         output = model(x)
-        # 손실함수 계산
         loss = criterion(output, y)
 
         optimizer.zero_grad()
@@ -73,18 +85,24 @@ for epoch in range(10000):
     if (epoch + 1) % 1000 == 0:
         print(f'Epoch: {epoch + 1:4d}, Model: {list(model.parameters())}, Cost: {cost:3f}')
 
-# no_grad는 자동 미분 설정 x로 추론에 적합한 상태로 변경
-# .eval()로 모델을 평가모드로 변경
-# .predict method에 가까운듯?
+# with torch.no_grad()와 함께라면, 자동미분 없는 검증 가능
 with torch.no_grad():
     model.eval()
-    inputs = torch.FloatTensor([[1 ** 2, 5], [5 ** 2, 5], [11 * 2, 11]]).to(device)
-    outputs = model(inputs)
-    print(outputs)
+    for x, y in test_dataloader:
+        x = x.to(device)
+        y = y.to(device)
+
+
+        outputs = model(x)
+        print(f'x: {x}')
+        print(f'y: {y}')
+        print(f'outputs: {outputs}')
+        print('--------------------------------')
 
 # 모델 저장과 특정 시점의 모델 저장
+'''
 torch.save(model, './models/model_14.pt')
 torch.save(model.state_dict(), './models/modeL-14_state_dict.pt')
+'''
 
-# device에 맞게 해주는건 layer, 손실함수, 텐서 전체에 해줘야한다
 # 108p
