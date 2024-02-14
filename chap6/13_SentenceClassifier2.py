7cd062d1223b43455ba331264701947d048c36a7
Calli-T <Calli-T@naver.com>
2024-02-14 22:45:14 +0900

    pip 24 update

--- chap6/13_SentenceClassifier2.py
+++ chap6/13_SentenceClassifier2.py
@@ -7,6 +7,6 @@ corpus_df = pd.DataFrame(corpus.test)
 train = corpus_df.sample(frac=0.9, random_state=42)
 test = corpus_df.drop(train.index)
 
-print(train.head(5).to_markdown()) # to_markdown은 dependency문제가 생긴다
+print(train.head(5).to_markdown()) # to_markdown은 dependency문제가 생긴다 -> 해결함 tabulate 설치
 print("Training Data Size: ", len(train))
 print("Tesing Data Size: ", len(test))
