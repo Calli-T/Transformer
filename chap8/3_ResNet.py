7de8454f18055a380e6efb4811623e5f079c4664
Calli-T <Calli-T@naver.com>
2024-03-25 21:12:40 +0900

    0325 2

--- chap8/3_ResNet.py
+++ chap8/3_ResNet.py
@@ -11,7 +11,7 @@ class ResNet(nn.Module):
         self.inplanes = 64
 
         self.stem = nn.Sequential(
-            nn.Conv2d(3, self.inplanes, kernel_size=7, stride=2, padding=3, bias=False), # RGB 3채널, 출력 차원은 64
+            nn.Conv2d(3, self.inplanes, kernel_size=7, stride=2, padding=3, bias=False),  # RGB 3채널, 출력 차원은 64
             nn.BatchNorm2d(self.inplanes),
             nn.ReLU(inplace=True),
             nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
@@ -23,16 +23,16 @@ class ResNet(nn.Module):
 
         self.avgpool = nn.AdaptiveAvgPool2d((1, 1))
         self.fc = nn.Linear(512 * block.expansion, num_classes)
-        
+
     def _make_layer(self, block, planes, num_blocks, stride):
-        layers =[]
+        layers = []
         layers.append(block(self.inplanes, planes, stride))
         self.inplanes = planes * block.expansion
         for _ in range(num_blocks - 1):
             layers.append(block(self.inplanes, planes, 1))
-            
+
         return nn.Sequential(*layers)
-    
+
     def forward(self, x):
         out = self.stem(x)
         out = self.stage1(out)
@@ -44,3 +44,25 @@ class ResNet(nn.Module):
         out = self.fc(out)
 
         return out
+
+
+# --------------------------------------------
+from torchvision import models
+from torchinfo import summary
+
+resnet18 = ResNet(BasicBlock, [2, 2, 2, 2], 1000)
+resnet34 = ResNet(BasicBlock, [3, 4, 6, 3], 1000)
+resnet50 = ResNet(BottleneckBlock, [3, 4, 6, 3], 1000)
+resnet101 = ResNet(BottleneckBlock, [3, 4, 23, 3], 1000)
+resnet152 = ResNet(BottleneckBlock, [3, 8, 36, 3], 1000)
+
+torch_resnet_model = models.resnet34(weights="ResNet34_Weights.IMAGENET1K_V1")
+
+resnet34_info = summary(resnet34, (1, 3, 224, 224), verbose=0)
+torch_model_info = summary(torch_resnet_model, (1, 3, 224, 224), verbose=0)
+
+print(resnet34_info.total_params)
+print(torch_model_info.total_params)
+
+resnet152_info = summary(resnet152, (1, 3, 224, 224), verbose=0)
+print(resnet152_info.total_params)
