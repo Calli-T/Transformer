a55821aaf8568b9df5dce0e3620da41dc5a6c18f
Calli-T <joy14479@gmail.com>
2024-05-16 10:48:42 +0900

    0516 1

--- TorchTest/DDPM/ddpm.py
+++ TorchTest/DDPM/ddpm.py
@@ -2,9 +2,6 @@ import torch
 
 from unet import *
 
-# hyper
-EMA = 0.999
-
 
 # 확산 스케줄, 오프셋 코사인 사용
 def offset_cosine_diffusion_schedule(diffusion_times):
@@ -22,17 +19,29 @@ def offset_cosine_diffusion_schedule(diffusion_times):
     return noise_rates, signal_rates
 
 
-class DDPM(nn.Module):
+class DDPM:
     def __init__(self, mean=None, std=None):
-        super().__init__()
 
         self.normalizer = nn.LayerNorm([3, IMAGE_SIZE, IMAGE_SIZE])  # 레이어 정규화, 샘플 각각에 대해서 수행함
         self.network = UNet()
         self.ema_network = UNet()
         self.diffusion_schedule = offset_cosine_diffusion_schedule
 
-        self.mean = mean
-        self.std = std
+        # hyper
+        self.EMA = 0.999
+        self.reverse_diffusion_steps = 20
+
+        self.mean = None
+        self.std = None
+
+        # if torch.is_tensor(mean):
+        #     self.mean = mean
+        # else:
+        #     self.mean = torch.tensor(mean)
+        # if torch.is_tensor(std):
+        #     self.std = std
+        # else:
+        #     self.std = torch.tensor(std)
 
     def denomalize(self, images):
         images = torch.clamp(self.mean + self.std * images, min=0, max=1)
@@ -69,20 +78,71 @@ class DDPM(nn.Module):
         return pred_noises, pred_images
 
     # 역방향 확산의 반복부터 다시 시작하면된다
-    def reverse_diffusion(self, initial_noise, diffusion_steps):
+    def reverse_diffusion(self, initial_noise, diffusion_steps=None):
+        if diffusion_steps is None:
+            diffusion_steps = self.reverse_diffusion_steps
+
         num_images = initial_noise.shape[0]
         step_size = 1.0 / diffusion_steps
         current_images = initial_noise
 
         for step in range(diffusion_steps):
             diffusion_times = [1 - step * step_size for _ in range(num_images)]
-            noise_rates, signal_rates = self.diffusion_schedule(torch.FloatTensor(diffusion_times))
+            noise_rates, signal_rates = self.diffusion_schedule(
+                torch.FloatTensor(diffusion_times)
+            )
 
             pred_noises, pred_images = self.denoise(
                 current_images, noise_rates, signal_rates, training=False
             )
             # print(pred_noises.shape)
             # print(pred_images.shape)
+
+            # next_diffusion_times = diffusion_times - step_size
+            next_diffusion_times = [x - step_size for x in diffusion_times]
+            next_noise_rates, next_signal_rates = self.diffusion_schedule(
+                torch.FloatTensor(next_diffusion_times)
+            )
+
+            '''
+            print(next_signal_rates.shape)
+            print(pred_images.shape)
+            print(next_noise_rates.shape)
+            print(pred_noises.shape)
+            '''
+            # current_images = (next_signal_rates * pred_images + next_noise_rates * pred_noises)
+            current_images = (next_signal_rates.view([-1, 1, 1, 1]) * pred_images +
+                              next_noise_rates.view([-1, 1, 1, 1]) * pred_noises)
+
+        return pred_images
+
+    def generate(self, num_images, diffusion_steps, initial_noise=None):
+        if initial_noise is None:
+            initial_noise = torch.randn(num_images, 3, IMAGE_SIZE, IMAGE_SIZE)
+        generated_images = self.reverse_diffusion(initial_noise, diffusion_steps)
+        # print(generated_images.shape)
+        generated_images = self.denomalize(generated_images)
+        # print(generated_images.shape)
+
+        return generated_images
+
+    '''
+    def set_mean_and_std(self, mean, std):
+        if torch.is_tensor(mean):
+            self.mean = mean
+        else:
+            self.mean = torch.tensor(mean)
+        if torch.is_tensor(std):
+            self.std = std
+        else:
+            self.std = torch.tensor(std)
+    '''
+
+    # 데이터로더, 평균, 표준 편차 모두 여기서 세팅
+    def set_datasets_from_path(self, path):
+        train_dataloader, mean, std = getDataLoader(path)
+        self.mean = torch.FloatTensor(mean)
+        self.std = torch.FloatTensor(std)
 
     '''
     def denoise_test(self):
@@ -102,36 +162,43 @@ class DDPM(nn.Module):
     '''
 
     # print(element.shape)
-
     '''
-        def normalize_test(self):
+    def normalize_test(self):
         train_dataloader, mean, std = getDataLoader("./datasets")
+
         self.mean = torch.FloatTensor(mean)
         self.std = torch.FloatTensor(std)
         for batch in train_dataloader:
             image = batch[0].permute(1, 2, 0).detach().numpy()
-            plt.imshow(image)
-            plt.show()
+            # plt.imshow(image)
+            # plt.show()
 
             return self.normalizer(batch)
     '''
 
     # 정방향 확산 프로세스(이건 함수가 따로 필요없고 훈련 때 바로 생성하는것도 가능함)
-    # 역방향 확산 프로세스(denoise함수 생성 완료, 그걸이용하는 reverse diffusion 함수 만들것)
-    # 생성 함수
     # 그리고 pytorch의 손실함수 선택, 최적화 알고리즘적용, 오차 역전파등의 학습과정
 
 
 ddpm = DDPM()
+ddpm.set_datasets_from_path("./datasets")
 
+# train_dataloader, mean_numpy, std_numpy = getDataLoader("./datasets")
+# ddpm = DDPM(mean=mean_numpy, std=std_numpy)
+# print(ddpm.generate(5, 20).shape)
+
+'''
+# 역방향 확산 테스트 코드
 # ddpm.reverse_diffusion(torch.FloatTensor([[[[1]]], [[[1]]], [[[1]]], [[[1]]], [[[1]]]]), 20)
 # 무작위 잡음의 생성은, [N, 3채널, 1, 1]를 생성하도록 함
 reverse_test_random_tensor = torch.rand([5, 3, 64, 64])
 # print(reverse_test_random_tensor.shape)
 # print(reverse_test_random_tensor)
 ddpm.reverse_diffusion(reverse_test_random_tensor, 20)
+'''
 
 # ddpm.denoise_test()
+
 
 '''
 # normalize_test와 denormalize코드 테스트용
@@ -139,7 +206,6 @@ images = ddpm.denomalize(ddpm.normalize_test()).detach().numpy()
 
 plt.imshow(images[0].swapaxes(0, 1).swapaxes(1, 2))
 plt.show()
-
 '''
 
 '''
