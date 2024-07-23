b49d17e553c55a7769dd2fbb2d5b9ada7014e462
Calli-T <joy14479@gmail.com>
2024-07-23 15:51:12 +0900

    0723 1

--- TorchTest/Git_Clone/IDDPM/scripts/show_np_images.py
+++ TorchTest/Git_Clone/IDDPM/scripts/show_np_images.py
@@ -4,6 +4,8 @@ import cv2
 from math import sqrt
 
 img_path = "./outputs/openai-2024-06-03-22-22-17-509729/samples_64x32x32x3.npz"
+img_path = "/tmp/openai-2024-07-16-10-51-03-359415/samples_16x256x256x3.npz"
+
 imgs = np.load(img_path, mmap_mode='r')
 
 # npz 인덱스 보기
@@ -12,6 +14,7 @@ imgs = np.load(img_path, mmap_mode='r')
 
 imgs = imgs['arr_0']
 print(imgs.shape[0])
+print(imgs.shape)
 
 # 아마도, 2^(even num)으로 나오면 상관없음
 wh = int(np.sqrt(imgs.shape[0]))
@@ -19,6 +22,6 @@ wh = int(np.sqrt(imgs.shape[0]))
 plt.figure(figsize=(wh, wh))
 for idx, image in enumerate(imgs):
     plt.subplot(wh, wh, idx + 1)
-    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    # image = cv2.cvtColor(image, cv2.COLOR_RGB2RGBA)#cv2.COLOR_BGR2RGB)
     plt.imshow(image)
 plt.show()
