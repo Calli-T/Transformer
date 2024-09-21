e650eb9e593daa5e107da964dc02400f52b9c1b9
Calli-T <joy14479@gmail.com>
2024-09-21 20:05:29 +0900

    0921 3

--- TorchTest/DiffSVC/PWG/download_model.py
+++ TorchTest/DiffSVC/PWG/download_model.py
@@ -5,4 +5,5 @@ for unit in dir(parallel_wavegan.utils):
     print(unit)
 '''
 from parallel_wavegan.utils import download_pretrained_model
-download_pretrained_model("vctk_parallel_wavegan.v1", "pretrained_model")
+# download_pretrained_model("vctk_parallel_wavegan.v1", "pretrained_model")
+download_pretrained_model("vctk_parallel_wavegan.v1.long", "pretrained_model")
--- TorchTest/DiffSVC/PWG/show_wave2.py
+++ TorchTest/DiffSVC/PWG/show_wave2.py
@@ -1,10 +1,10 @@
 import librosa.display, librosa
 from matplotlib import pyplot as plt
 
-sig, sr = librosa.load('./sample/cs1_gen.wav')
-print(sr)
+# sig, sr = librosa.load('./sample/L-O-V-E.wav')
+sig, sr = librosa.load('./outputs/L-O-V-E_gen.wav')
 plt.figure()
-librosa.display.waveshow(sig, alpha=0.5)
+librosa.display.waveshow(sig, sr=sr, alpha=0.5)
 plt.xlabel("Time (s)")
 plt.ylabel("Amplitude")
 plt.title("Waveform")
