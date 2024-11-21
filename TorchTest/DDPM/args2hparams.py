d6ea68a3152e25fdcbaf94ae01caa4123c3300b6
Calli-T <joy14479@gmail.com>
2024-11-21 23:32:27 +0900

    1121 5

--- TorchTest/DDPM/args2hparams.py
+++ TorchTest/DDPM/args2hparams.py
@@ -58,7 +58,7 @@ def get_default():
         "save_interval": 4000,
         "EMA": 0.999,
         # for sampling
-        "BATCH_SIZE_SAMPLE": 8,
+        "BATCH_SIZE_SAMPLE": 16,
         # for diffusion
         "schedule_name": "linear",
         "steps": 1000,
