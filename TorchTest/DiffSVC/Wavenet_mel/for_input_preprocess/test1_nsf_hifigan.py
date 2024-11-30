194df13a860094403690514ccf884df277875866
Calli-T <joy14479@gmail.com>
2024-11-30 15:28:09 +0900

    quick save 1130

--- TorchTest/DiffSVC/Wavenet_mel/for_input_preprocess/test1_nsf_hifigan.py
+++ TorchTest/DiffSVC/Wavenet_mel/for_input_preprocess/test1_nsf_hifigan.py
@@ -2,6 +2,5 @@ from NsfHiFiGAN.nsf_hifigan import NsfHifiGAN
 from temp_hparams import hparams
 
 vocoder = NsfHifiGAN(hparams)
-# wav, mel = VOCODERS[hparams['vocoder']].wav2spec(temp_dict['wav_fn'])
 wav, mel = vocoder.wav2spec('./raw/L-O-V-E.wav', hparams)
 print(wav.shape, mel.shape)
