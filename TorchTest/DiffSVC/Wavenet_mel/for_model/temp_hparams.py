6df9073b55566f21d11f52a70ede76c6708ba3b5
Calli-T <joy14479@gmail.com>
2024-12-07 17:43:51 +0900

    1207 5 wavenet model ckpt 2 pt end

--- TorchTest/DiffSVC/Wavenet_mel/for_model/temp_hparams.py
+++ TorchTest/DiffSVC/Wavenet_mel/for_model/temp_hparams.py
@@ -1,3 +1,7 @@
+from torch import device, cuda
+device = device('cuda' if cuda.is_available() else 'cpu')
+epoch = 38100
+project_name = "SinChangSeop"
 # class DiffNet에서 사용하는 hparams를 임시로 가져온것
 hparams = {
     "hidden_size": 256,
@@ -5,4 +9,6 @@ hparams = {
     "residual_channels": 384,
     "dilation_cycle_length": 4,
     "audio_num_mel_bins": 128,
+    "device": device,
+    "wavenet_model_path": f"{project_name}/wavenet_model_steps_{epoch}.pt",
 }
--- TorchTest/DiffSVC/Wavenet_mel/for_model/test1.py
+++ TorchTest/DiffSVC/Wavenet_mel/for_model/test1.py
@@ -3,22 +3,25 @@ from temp_hparams import hparams
 from temp_load_ckpt import load_ckpt
 
 import torch
-from torch import device, cuda
-
-device = device('cuda' if cuda.is_available() else 'cpu')
+# from torch import device, cuda
+#
+# device = device('cuda' if cuda.is_available() else 'cpu')
 
 wavenet_mel = DiffNet(hparams)
-load_ckpt(wavenet_mel, 'SinChangSeop/model_ckpt_steps_30000.ckpt')
+# load_ckpt(wavenet_mel, 'SinChangSeop/model_ckpt_steps_30000.ckpt')
+wavenet_mel.load_state_dict(torch.load(hparams['wavenet_model_path'], map_location='cpu'))
 # print(wavenet_mel)
-wavenet_mel.to(device)
+wavenet_mel.to(hparams['device'])
 '''
 :param spec: [B, 1, M, T]
         :param diffusion_step: [B, 1] -> [B, ] 실제로는 1축 텐서이니 앞의 ,1은 무시할것
         :param cond: [B, M, T] -> 이거 M이 mel-band 수가 아니고, hidden 값과 관련있는 것 같다
 '''
-'''
-temp_spec = torch.randn((2, 1, 128, 1)).to(device)
-temp_step = torch.randint(0, 1000, (2,)).to(device)
-temp_cond = torch.randn((2, 256, 1)).to(device)
-print(wavenet_mel(temp_spec, temp_step, temp_cond))
-'''
+
+temp_spec = torch.randn((2, 1, 128, 1)).to(hparams['device'])
+temp_step = torch.randint(0, 1000, (2,)).to(hparams['device'])
+temp_cond = torch.randn((2, 256, 1)).to(hparams['device'])
+print(temp_spec.shape)
+print(temp_step.shape)
+print(temp_cond.shape)
+print(wavenet_mel(temp_spec, temp_step, temp_cond).shape)
