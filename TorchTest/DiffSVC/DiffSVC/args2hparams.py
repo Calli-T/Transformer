0c864ec93bc1939ec725319dc630530208ede02e
Calli-T <joy14479@gmail.com>
2024-12-31 21:15:23 +0900

    1231 2 legacy code separated

--- TorchTest/DiffSVC/DiffSVC/args2hparams.py
+++ TorchTest/DiffSVC/DiffSVC/args2hparams.py
@@ -10,7 +10,7 @@ hparams = {
     "model_pt_epoch": pt_epoch,  # 학습'된' epoch
 
     "raw_wave_path": "raw/L-O-V-E-[cut_12sec].wav",  # "raw/L-O-V-E_[cut_6sec].wav",
-    # "raw_wave_dir_path": "raw",
+    "raw_wave_dir_path": "raw/source",
 
     # for vocoder, NsfHiFiGAN
     # "vocoder": "nsf_hifigan.NsfHifiGAN",
